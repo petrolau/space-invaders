4fd1e119782b6ccc337eceb82de132812ed62603
Laura Maria Alencar Leal Petrola <46495368+ptrlauu@users.noreply.github.com>
2020-05-27 18:35:01 -0300

    mais funções

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pygame 
+import math
 import random
+
+import pygame
+from pygame import mixer
 
 #Inicia o pygame
 pygame.init()
@@ -8,27 +11,41 @@ pygame.init()
 screen = pygame.display.set_mode((800,600))
 
 #Background
+
 background = pygame.image.load('background.png')
 
 #Titulo e Icone
+
 pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
 #Player
+
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
 playerX_change = 0
 
 #Enemy
-enemyImg = pygame.image.load('enemy.png')
-enemyX = random.randint(0, 800)
-enemyY = random.randint(50, 150)
-enemyX_change = 2
-enemyY_change = 40
+
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change = []
+num_of_enemies = 6
+
+#atribuindo todas as caracteristicas aos n inimigos
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
+    enemyX.append(random.randint(0, 736))
+    enemyY.append(random.randint(50, 150))
+    enemyX_change.append(2)
+    enemyY_change.append(40)
 
 #Bullet
+
 bulletImg = pygame.image.load('bullet.png')
 bulletX = 0
 bulletY = 480
@@ -38,18 +55,38 @@ bulletY_change = 10
 #Fire -> A bala esta se movendo
 bullet_state = "ready"
 
+#Score
+score_value = 0
+font = pygame.font.Font('freesansbold.ttf',32)
+
+textX = 10
+textY = 10
+
+def show_score(x, y):
+    score = font.render("Score : " + str(score_value), True, (255,255,255))
+    screen.blit(score,(x, y))
+
+
 def player(x, y):
     #desenhando a imagem do player
     screen.blit(playerImg,(x, y))
 
-def enemy(x, y):
-    screen.blit(enemyImg, (x, y))
+def enemy(x, y, i):
+    screen.blit(enemyImg[i], (x, y))
 
 def fire_bullet(x,y):
     global bullet_state
     bullet_state = "fire"
     screen.blit(bulletImg, (x + 16, y + 10))
 
+def is_collision(enemyX, enemyY, bulletX, bulletY):
+    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
+    
+         
 #Game Loop
 running = True
 while running:
@@ -65,20 +102,23 @@ while running:
 
         # checando se estamos pressionando para direita ou esquerda no teclado
         if event.type == pygame.KEYDOWN:
-            print("A keystoke is pressed")
             if event.key == pygame.K_LEFT:
                 playerX_change = -5
             if event.key == pygame.K_RIGHT:
                 playerX_change = 5
             if event.key == pygame.K_SPACE:
-                fire_bullet(playerX,bulletY)
+                if bullet_state == "ready":
+                    #pega a coordenada atual x da nave
+                    bulletX = playerX
+                    fire_bullet(bulletX,bulletY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
                 
 
-    # Chechando pelos limites da tela, evitando que a nave e o inimigo saiam da tela.
+    # Checando pelos limites da tela, evitando que a nave e o inimigo saiam da tela.
+    
     playerX += playerX_change
 
     if playerX <= 0:
@@ -89,27 +129,48 @@ while running:
 
     
     #Movimento do inimigo.
-    enemyX += enemyX_change
 
-    if enemyX <= 0:
-        enemyX_change = 2
-        enemyY+= enemyY_change
+    for i in range (num_of_enemies):
+        enemyX[i] += enemyX_change[i]
+
+        if enemyX[i] <= 0:
+            enemyX_change[i] = 2
+            enemyY[i]+= enemyY_change[i]
+        elif enemyX[i] >= 736:
+            enemyX_change[i] = -2
+            enemyY[i] += enemyY_change[i]
+
+        #Colisão
+
+        collision = is_collision(enemyX[i],enemyY[i],bulletX,bulletY)
+        if collision:
+            bulletY = 480
+            bullet_state = "ready"
+            score_value += 1
+            enemyX[i] = random.randint(0, 735)
+            enemyY[i] = random.randint(50, 150)
+
+        enemy(enemyX[i], enemyY[i], i)
+ 
 
     #Movimento da bala.
-    if bullet_state is "fire":
-        fire_bullet(playerX,bulletY)
+
+    if bulletY <= 0:
+        bulletY = 480
+        bullet_state = "ready"
+
+    if bullet_state == "fire":
+        fire_bullet(bulletX,bulletY)
         bulletY -= bulletY_change
-        
+
     #800-64 (largura da nave)
-    elif enemyX >= 736:
-        enemyX_change = -2
-        enemyY+= enemyY_change
+    elif enemyX[i] >= 736:
+        enemyX_change[i] = -2
+        enemyY[i]+= enemyY_change[i]
 
-        
 
-    
 
     player(playerX,playerY)
-    enemy(enemyX, enemyY)
+    show_score(textX,textY)
     pygame.display.update()
             
